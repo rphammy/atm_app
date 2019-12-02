fc7b7bd09109f8c5cbd7ef1b7c65d0cd78872393
Rachel Pham <42249215+rphammy@users.noreply.github.com>
2019-12-01 22:42:54 -0800

    Add files via upload

---
+++ insert_customers.sql
@@ -0,0 +1,64 @@
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(361721022, 'Alfred Hitchcock', '6667 El Colegio #40', '1234'); 
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES (231403227, 'Billy Clinton','5777 Hollister' ,'1468');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(412231856, 'Cindy Laugher', '7000 Hollister', '3764');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(207843218, 'David Copperfill','1357 State St', '8582');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(122219876, 'Elizabeth Sailor', '4321 State St', '3856');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(401605312, 'Fatal Castro','3756 La Cumbre Plaza' ,'8193');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(201674933, 'George Brush', '5346 Foothill Av', '9824');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(212431965, 'Hurryson Ford', '678 State St', '3532');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(322175130, 'Ivan Lendme','1235 Johnson Dr', '8471');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(344151573, 'Joe Pepsi','3210 State St' ,'3692');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(209378521, 'Kelvin Costner','Santa Cruz #3579', '4659');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(212116070, 'Li Kung', '2 People''s Rd Beijing', '9173');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(188212217, 'Magic Jordon','3852 Court Rd', '7351');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(203491209, 'Nam-Hoi Chung', '1997 People''s St HK', '5340');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(210389768, 'Olive Stoner', '6689 El Colegio #151', '8452');
+INSERT INTO Customers 
+(taxid, cname, address, pinkey) 
+VALUES 
+(400651982, 'Pit Wilson', '911 State St' ,'1821');
+
